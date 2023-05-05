69445bb088c712292054bae45379104e8d1437b2
shalforb <shalford@live.com.au>
2023-05-05 14:46:47 +1000

    added more json file handling

--- watchlist.py
+++ watchlist.py
@@ -12,3 +12,13 @@ def load_watchlist():
 def save_watchlist(watchlist):
     with open("watchlist.json", "w") as f:
         json.dump(watchlist, f, indent=4)
+
+# Function to save the watchlist to a JSON file
+def save_watchlist(watchlist):
+    with open("watchlist.json", "w") as f:
+        json.dump(watchlist, f, indent=4)
+
+# Function to save the watched list to a JSON file
+def save_watched_list(watched_list):
+    with open("watched.json", "w") as f:
+        json.dump(watched_list, f, indent=4)
