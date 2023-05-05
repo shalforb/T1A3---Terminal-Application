c8744d79b9f18edda9a0f92a4a4358b89d93d851
shalforb <shalford@live.com.au>
2023-05-05 16:46:15 +1000

    edited watchlist code

--- filmwatch.py
+++ filmwatch.py
@@ -15,9 +15,3 @@ def save_watchlist(watchlist):
     with open(WATCHLIST_FILE, 'w') as f:
         json.dump(watchlist, f, indent=2)
 
-def add_film():
-    title = input('Enter the title of the film: ')
-    genre = input('Enter the genre of the film: ')
-    release_date = input('Enter the release date of the film: ')
-    watchlist.append({'title': title, 'genre': genre, 'release_date': release_date})
-    save_watchlist(watchlist)
