e8736bd6525f389dd67910ee84d254c44d61dbf8
shalforb <shalford@live.com.au>
2023-05-06 11:28:13 +1000

    added extra print by genre/year functions, added extra comments

--- filmwatch.py
+++ filmwatch.py
@@ -3,6 +3,7 @@ import json
 WATCHLIST_FILE = 'watchlist.json'
 WATCHED_FILE = 'watched.json'
 
+# Function to load the watchlist from a JSON file
 def load_watchlist():
     try:
         with open(WATCHLIST_FILE, 'r') as f:
@@ -11,12 +12,14 @@ def load_watchlist():
         watchlist = []
     return watchlist
 
+# Function to load the watched list from a JSON file
 def save_watchlist(watchlist):
     with open(WATCHLIST_FILE, 'w') as f:
         json.dump(watchlist, f, indent=2)
 
-watchlist = load_watchlist()
 
+
+# Function to add a film to the watchlist
 def add_film():
     global watchlist # define the watchlist variable as a global variable
     title = input('Enter the title of the film: ')
@@ -25,6 +28,7 @@ def add_film():
     watchlist.append({'title': title, 'genre': genre, 'release_date': release_date})
     save_watchlist(watchlist)
 
+# Function to edit a film in the watchlist
 def edit_film():
     global watchlist # define the watchlist variable as a global variable
     title = input('Enter the title of the film to edit: ')
@@ -36,6 +40,7 @@ def edit_film():
             return
     print(f'"{title}" not found in watchlist')
 
+# Function to remove a film from the watchlist
 def remove_film():
     global watchlist # define the watchlist variable as a global variable
     title = input('Enter the title of the film to remove: ')
@@ -46,10 +51,30 @@ def remove_film():
             return
     print(f'"{title}" not found in watchlist')
 
+# Function to print the watchlist
 def print_watchlist():
+    global watchlist # define the watchlist variable as a global variable
     print('Watchlist:')
     for film in watchlist:
         print(f"{film['title']} ({film['genre']}, {film['release_date']})")
+
+# Function to print films by genre
+def print_genre(genre):
+    global watchlist # define the watchlist variable as a global variable
+    print(f'Watchlist ({genre}):')
+    for film in watchlist:
+        if film['genre'] == genre:
+            print(f"{film['title']} ({film['release_date']})")
+
+# Function to print films by release date
+def print_release_date():
+    global watchlist # define the watchlist variable as a global variable
+    print('Watchlist (sorted by release date):')
+    for film in sorted(watchlist, key=lambda x: x['release_date']):
+        print(f"{film['title']} ({film['genre']}, {film['release_date']})")
+
+#Call the load_watchlist function and assign the result to the watchlist variable
+watchlist = load_watchlist()
 
 while True:
     print('Welcome to the FilmApp')
@@ -73,6 +98,11 @@ while True:
         remove_film()
     elif choice == '4':
         print_watchlist()
+    elif choice == '5':
+        genre = input('Enter the genre to filter by: ')
+        print_genre(genre)
+    elif choice == '6':
+        print_release_date()
     elif choice == '8':
         print('Goodbye!')
         break
