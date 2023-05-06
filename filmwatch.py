10282575189645083b238e8c9192686cf1a4d19c
shalforb <shalford@live.com.au>
2023-05-06 10:51:04 +1000

    added draft of main menu

--- filmwatch.py
+++ filmwatch.py
@@ -40,3 +40,29 @@ def remove_film():
             save_watchlist(watchlist)
             return
     print(f'"{title}" not found in watchlist')
+
+while True:
+    print('Welcome to the FilmApp')
+    print('-----------------------------')
+    print('1. Add film')
+    print('2. Edit film')
+    print('3. Remove film')
+    print('4. Print watchlist')
+    print('5. Print by genre')
+    print('6. Print by release date')
+    print('7. Mark film as watched')
+    print('8. Exit')
+
+    choice = input('Enter your choice (1-8): ')
+
+    if choice == '1':
+        add_film()
+    elif choice == '2':
+        edit_film()
+    elif choice == '3':
+        remove_film()
+    elif choice == '8':
+        print('Goodbye!')
+        break
+    else:
+        print('Invalid choice, please try again.')
